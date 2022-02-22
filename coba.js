87def1c9a7e3bb891af663e857ce59953cabbb06
Yusriyah IM <yusriyahisna24@gmail.com>
2022-02-22 21:55:32 +0700

    add code snippet doest allow inspection

---
+++ coba.js
@@ -0,0 +1,11 @@
+// int main(void) { int currectScore = 50; int highScore = 80; if(currectScore > highScore){ printf("Selamat, Anda meraih highscore"); }else{ printf("Coba lagi ya"); } return 0; }
+
+int currectScore = 50;
+    int highScore =80;
+   
+   if(currectScore >                     highScore){
+       printf(      "Selamat, Anda meraih highscore");
+   }
+   else{
+                   printf("Coba lagi ya");
+   }
